c4b8e0ba00a65340a620053a5847955b0106ded9
marcelot <marcelojtelles@gmail.com>
2019-06-13 12:54:00 -0300

    Add files via upload

--- js/scriptanimate.js
+++ js/scriptanimate.js
@@ -15,3 +15,23 @@ $(function() {
       }, 2250 );
     }
   });
+  
+  var animateButton = function(e) {
+
+  e.preventDefault;
+  //reset animation
+  e.target.classList.remove('animate');
+  
+  e.target.classList.add('animate');
+  
+  e.target.classList.add('animate');
+  setTimeout(function(){
+    e.target.classList.remove('animate');
+  },6000);
+};
+
+var classname = document.getElementsByClassName("button1");
+
+for (var i = 0; i < classname.length; i++) {
+  classname[i].addEventListener('click', animateButton, false);
+}
